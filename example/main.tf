cee5348fd9c6f9e7f07e39864c8bc860e8388b16
Luis Mayta <luis@hadenlabs.com>
2021-01-14 02:47:44 -0500

    refactor: required providers and versions to file versions (#9)

--- example/main.tf
+++ example/main.tf
@@ -1,30 +1,3 @@
-terraform {
-  required_version = ">=0.13.0"
-  required_providers {
-
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">=3.2.0"
-    }
-
-    template = {
-      source  = "hashicorp/template"
-      version = ">=1.0.0"
-    }
-
-    null = {
-      source  = "hashicorp/null"
-      version = ">=0.1.0"
-    }
-
-    local = {
-      source  = "hashicorp/local"
-      version = ">=1.3.0"
-    }
-
-  }
-}
-
 module "main" {
   source = "git://github.com/hadenlabs/terraform-aws-openvpn.git?ref=0.1.0"
   providers = {
@@ -35,5 +8,4 @@ module "main" {
   region         = var.aws_region
   admin_user     = "slovacus"
   admin_password = "password-sensible implement with sops"
-
 }
