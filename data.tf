450773f256e8a65eef8f6b11f2ed8d7b7e91bf24
@slovacus <slovacus@gmail.com>
2021-02-04 17:51:43 -0500

    refactor: execute commands template for install and configure terraform (#20)

--- data.tf
+++ data.tf
@@ -23,3 +23,32 @@ data "aws_ami" "amazon_linux" {
 
   owners = ["099720109477"]
 }
+
+data "template_file" "vpn_install" {
+  template = file(format("%s/%s", path.module, "scripts/openvpn_install.sh.tpl"))
+
+  vars = {
+    public_ip  = aws_instance.openvpn.public_ip
+    public_dns = aws_instance.openvpn.public_dns
+    client     = var.admin_user
+  }
+}
+
+data "template_file" "vpn_update_user" {
+  template = file(format("%s/%s", path.module, "scripts/openvpn_update_user.sh.tpl"))
+
+  vars = {
+    client = var.admin_user
+  }
+}
+
+data "template_file" "vpn_download_keys" {
+  template = file(format("%s/%s", path.module, "scripts/openvpn_download_keys.sh.tpl"))
+
+  vars = {
+    ssh_user     = var.ssh_user
+    storage_path = var.storage_path
+    public_ip    = aws_eip.openvpn.public_ip
+    private_key  = var.private_key
+  }
+}
