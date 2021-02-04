d82dc1eb9c488bb33b6fede2fd539d61732b5c35
Luis Mayta <luis@hadenlabs.com>
2021-02-04 17:51:43 -0500

    refactor: execute commands template for install and configure terraform (#20)

--- commands.tf
+++ commands.tf
@@ -35,23 +35,7 @@ resource "null_resource" "openvpn_install" {
   }
 
   provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update -y",
-      "curl -O https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh",
-      "chmod +x openvpn-install.sh",
-      <<EOT
-          sudo AUTO_INSTALL=y \
-            APPROVE_INSTALL=y \
-            APPROVE_IP=${aws_instance.openvpn.public_ip} \
-            ENDPOINT=${aws_instance.openvpn.public_dns} \
-            IPV6_SUPPORT=n \
-            PORT_CHOICE=1 \
-            CLIENT=${var.admin_user} \
-            DNS=1 \
-           ./openvpn-install.sh
-EOT
-      ,
-    ]
+    inline = [data.template_file.vpn_install.rendered]
   }
 }
 
@@ -73,11 +57,7 @@ resource "null_resource" "openvpn_adduser" {
   }
 
   provisioner "remote-exec" {
-    inline = [
-      "curl -O https://raw.githubusercontent.com/dumrauf/openvpn-terraform-install/master/scripts/update_users.sh",
-      "chmod +x ~/update_users.sh",
-      format("sudo ~/update_users.sh %s", var.admin_user),
-    ]
+    inline = [data.template_file.vpn_update_user.rendered]
   }
 }
 
@@ -91,12 +71,7 @@ resource "null_resource" "openvpn_download_configurations" {
   depends_on = [null_resource.openvpn_adduser]
 
   provisioner "local-exec" {
-    command = <<EOT
-    mkdir -p ${var.storage_path}/${aws_eip.openvpn.public_ip};
-    scp -o StrictHostKeyChecking=no \
-        -o UserKnownHostsFile=/dev/null \
-        -i ${var.private_key} ${var.ssh_user}@${aws_eip.openvpn.public_ip}:/home/${var.ssh_user}/*.ovpn ${var.storage_path}/${aws_eip.openvpn.public_ip}/
-EOT
+    command = data.template_file.vpn_download_keys.rendered
 
   }
 }
