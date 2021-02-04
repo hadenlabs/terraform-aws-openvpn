023ad7bd2d225e2824f527f734261716cc2bce6a
@slovacus <slovacus@gmail.com>
2021-02-04 10:43:41 -0500

    fix: change output public ip of aws eip (#18)

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "instance" {
   description = "return instance openvpn"
-  value = aws_instance.openvpn
+  value       = aws_instance.openvpn
 }
 
 output "instance_ip" {
-  description = "return instance openvpn public ip"
-  value = aws_instance.openvpn.public_ip
+  description = "return instance openvpn elastic ip public"
+  value       = aws_eip.openvpn.public_ip
 }
 
 output "private_key" {
   description = "return filepath privatekey"
-  value = var.private_key
+  value       = var.private_key
 }
