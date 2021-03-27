8e55d7ce551691c17021b21ec3cde3fe8dd0652c
@slovacus <slovacus@gmail.com>
2021-03-27 16:34:03 -0500

    refactor: docs examples (#21)

--- provision/make/docs.mk
+++ provision/make/docs.mk
@@ -8,19 +8,16 @@ docs:
 docs.help:
 	@echo '    Docs:'
 	@echo ''
-	@echo '        docs.show                  Show mkdocs'
-	@echo '        docs.build                 build mkdocs'
-	@echo '        docs.terraform             generated docs for terraform'
+	@echo '        docs.show                  Show grip readme'
+	@echo '        docs.build                 Show mkdocs'
+	@echo '        docs.serve                 server Make documentation'
 	@echo ''
 
-docs.terraform:
-	$(call terraform-docs, ${TERRAFORM_README_FILE}, \
-			'This document gives an overview of variables used in the platform of the ${PROJECT}.', \
-			variables.tf)
-
 docs.show:
-	$(PIPENV_RUN) mkdocs serve
+	$(PIPENV_RUN) grip
 
 docs.build:
 	$(PIPENV_RUN) mkdocs build
 
+docs.serve:
+	$(PIPENV_RUN) mkdocs serve
