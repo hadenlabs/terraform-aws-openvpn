f0b419a69b07b69a537e9038ea61324a9f94248f
@slovacus <slovacus@gmail.com>
2021-03-27 16:34:57 -0500

    refactor: actions make (#21)

--- provision/make/tests.mk
+++ provision/make/tests.mk
@@ -2,6 +2,7 @@ test.help:
 	@echo '    Tests:'
 	@echo ''
 	@echo '        test                      show help'
+	@echo '        test.all   	             Run all module test'
 	@echo '        test.lint                 Run all pre-commit'
 	@echo ''
 
@@ -9,5 +10,10 @@ test:
 	@echo $(MESSAGE) Running tests on the current Python interpreter with coverage $(END)
 	make test.help
 
+test.all:
+	@echo $(MESSAGE) Running all tests $(END)
+	make test.lint
+
 test.lint:
+	@echo $(MESSAGE) Running validator for pre-commit $(END)
 	$(PIPENV_RUN) pre-commit run --all-files --verbose
