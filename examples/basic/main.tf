05d6338c6232717f169a52fbd61955f3ba6ca51a
@slovacus <slovacus@gmail.com>
2021-05-02 16:06:46 -0500

    refactor: example to examples (#25)

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,10 +1,10 @@
 module "main" {
-  source = "git://github.com/hadenlabs/terraform-aws-openvpn.git?ref=0.2.0"
   providers = {
     aws      = aws.main
     template = template
     local    = local
   }
+  source       = "../"
   public_key   = "/etc/keys/pub/key.pub"
   private_key  = "/etc/keys/pub/key.pem"
   admin_user   = "slovacus"
