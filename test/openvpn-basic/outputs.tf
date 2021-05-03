132d4fd62a2991a5e17b3619d677b15b25680286
Luis Mayta <luis@hadenlabs.com>
2021-05-03 00:44:06 -0500

    test: implement openvpn basic (#27)

---
+++ test/openvpn-basic/outputs.tf
@@ -0,0 +1,9 @@
+output "instance" {
+  description = "show instance module"
+  value       = module.main.instance
+}
+
+output "instance_ip" {
+  description = "show ip of instance"
+  value       = module.main.instance_ip
+}
