7438cb833043dc8d3cd62b2d5cf5ced429936351
@slovacus <slovacus@gmail.com>
2021-05-03 02:35:07 -0500

    refactor: test openvpn basic (#29)

--- test/openvpn-basic/variables.tf
+++ test/openvpn-basic/variables.tf
@@ -12,6 +12,32 @@
 # OPTIONAL VARIABLES
 # These variables have defaults, but may be overridden.
 # ---------------------------------------------------------------------------------------------------------------------
+variable "namespace" {
+  type        = string
+  description = "Namespace, which could be your organization name or abbreviation, e.g. 'eg' or 'cp'"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment, e.g. 'uw2', 'us-west-2', OR 'prod', 'staging', 'dev', 'UAT'"
+}
+
+variable "stage" {
+  type        = string
+  description = "Stage, e.g. 'prod', 'staging', 'dev', OR 'source', 'build', 'test', 'deploy', 'release'"
+}
+
+variable "name" {
+  type        = string
+  description = "Solution name, e.g. 'app' or 'jenkins'"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`"
+  default     = {}
+}
+
 
 variable "aws_region" {
   description = "region of aws"
