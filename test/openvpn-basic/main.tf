160578bc3534280d9a508e8793d6e57607a2fe68
@slovacus <slovacus@gmail.com>
2021-05-03 02:35:07 -0500

    refactor: test openvpn basic (#29)

--- test/openvpn-basic/main.tf
+++ test/openvpn-basic/main.tf
@@ -1,5 +1,10 @@
 module "main" {
   source       = "../.."
+  namespace    = var.namespace
+  environment  = var.environment
+  stage        = var.stage
+  name         = var.name
+  tags         = var.tags
   public_key   = var.public_key
   private_key  = var.private_key
   admin_user   = var.admin_user
